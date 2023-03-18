0bc540b7e9e4c462d098607cfe91fd1a61359785
lucaassp <lucaspereira.student@gmail.com>
2023-03-17 22:37:49 -0300

    Project started

---
+++ backend/technical-support-center/src/main/java/com/supportcenter/technicalsupportcenter/TechnicalSupportCenterApplication.java
@@ -0,0 +1,13 @@
+package com.supportcenter.technicalsupportcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalSupportCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalSupportCenterApplication.class, args);
+	}
+
+}
---
+++ backend/technical-support-center/src/test/java/com/supportcenter/technicalsupportcenter/TechnicalSupportCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportcenter.technicalsupportcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalSupportCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
